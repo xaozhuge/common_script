62a36a1d3aa4e568cccd38913c82695ccd187d42
zzz <zzz@qimingpian.com>
2021-05-11 17:58:54 +0800

    过滤 mysql 和 压缩的输出

--- syncToiCloud.sh
+++ syncToiCloud.sh
@@ -1,4 +1,8 @@
 #/bin/bash
+#定义函数
+removewarning(){
+    grep -v '[Warning] Using a password on the command line interface can be insecure.'
+}
 #备份时间
 time=`date +%Y%m%d%H`
 #备份数据库名称
@@ -12,10 +16,11 @@ tar_filename=$time.zip
 #server.txt 格式化为 localmysql:username:password
 read username password <<< `cat server.txt |grep localmysql|awk -F ':' '{print $2,$3}'`
 #备份数据库
-/usr/local/mysql/bin/mysqldump -u$username -p$password $databasename  > $basepath$local_filename
+/usr/local/mysql/bin/mysqldump -u$username -p$password $databasename  > $basepath$local_filename 2>&1 | removewarning
 #压缩文件
-/usr/bin/zip -jr $basepath$tar_filename $basepath$local_filename
+/usr/bin/zip -jr $basepath$tar_filename $basepath$local_filename 2>&1 | grep -v deflated
 #移除压缩前的文件
 rm -f $basepath$local_filename
 #移动到icloud目录
 /bin/mv $basepath$tar_filename /Users/peiwei/Library/Mobile\ Documents/com~apple~CloudDocs
+du -sh /Users/peiwei/Library/Mobile\ Documents/com~apple~CloudDocs/$tar_filename
