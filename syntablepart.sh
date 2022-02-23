62279c436af080c9a2fdefeab43aa3ab96248c11
zzz <zzz@qimingpian.com>
2022-02-23 13:58:42 +0800

    同步数据表部分内容到本地

---
+++ syntablepart.sh
@@ -0,0 +1,26 @@
+#/bin/bash
+#定义函数
+removewarning(){
+    grep -v '[Warning] Using a password on the command line interface can be insecure.'
+}
+
+#数据库名称
+databases=$1
+#数据表名称
+tables=$2
+#数据表文件保存路径
+tableSqlPath=/tmp/table-$(date +%Y%m%d%H).sql
+
+
+#server.txt 格式化为 localmysql:username1:password1
+read username1 password1 <<< `cat server.txt |grep localmysql|awk -F ':' '{print $2,$3}'`
+#server.txt 格式化为 remotemysql:username2:password2:ip2:port2
+read username2 password2 ip2 port2 <<< `cat server.txt |grep remotemysql|awk -F ':' '{print $2,$3,$4,$5}'`
+
+#sql文件初始化，切换数据库
+echo 'use '${databases}';' > $tableSqlPath
+#备份数据表 --no-create-info 表示取消创建表sql，默认存在 --where where条件限制
+/usr/local/mysql/bin/mysqldump -h$ip2 -u$username2 -P$port2 -p$password2 --no-create-info  $databases $tables --where="id >= 10 and id < 20 " >> $tableSqlPath 2>&1 | removewarning
+
+#导入本地表
+/usr/local/mysql/bin/mysql -u$username1 -p$password1 < $tableSqlPath
