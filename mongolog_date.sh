0765a4afb7fcddbefa3720cd17d01cf9cca646f4
zzz <xaozhuge@163.com>
2022-09-26 10:00:45 +0800

    过滤

--- mongolog_date.sh
+++ mongolog_date.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 #读取账号和密码
-read username password <<< `cat /tencentdata/www/script.qimingpian.com/server.txt |grep mongodb|awk -F ':' '{print $2,$3}'`
+read username password <<< `cat /tencentdata/www/script.com/server.txt |grep mongodb|awk -F ':' '{print $2,$3}'`
 #认证 => logRotate
 /usr/local/kencery/mongoDB/mongodb/bin/mongo 127.0.0.1:27017/admin --eval "db.auth('$username','$password');db.runCommand({logRotate:1});"
